ef96cf34a8131ade703b47b89ce4330aabdc1ef7
Oliver <oliver.petschick@gmail.com>
2020-12-05 14:30:54 +0100

    Solution q1

--- tracking/inference.py
+++ tracking/inference.py
@@ -179,7 +179,17 @@ class InferenceModule:
         Return the probability P(noisyDistance | pacmanPosition, ghostPosition).
         """
         "*** YOUR CODE HERE ***"
-        raiseNotDefined()
+        if ghostPosition == jailPosition:
+            if noisyDistance == None:
+                return 1.0
+            else:
+                return 0.0
+        if noisyDistance == None:
+            return 0.0
+
+        trueDistance = manhattanDistance(pacmanPosition, ghostPosition)
+        return busters.getObservationProbability(noisyDistance, trueDistance) 
+
 
     def setGhostPosition(self, gameState, ghostPosition, index):
         """
